9317f1cfd4f96c3977ce44cb0b505532ee27fe2d
Petar Strinic <petar@sofea.net>
2011-12-18 15:06:47 -0600

    Missed file

---
+++ spec/js/NotWrapped.js
@@ -0,0 +1,7 @@
+if(typeof window.sofea === 'undefined') window.sofea = {};
+if(typeof window.sofea.include === 'undefined') window.sofea.include = {};
+if(typeof window.sofea.include.NotWrapped === 'undefined') window.sofea.include.NotWrapped = {};
+
+window.sofea.include.NotWrapped.getTrue = function() {
+    return true;
+}
