65ac86f16dba985e95c794e02eb863292c6a11c7
Jamon Terrell <jamon@sofea.net>
2011-12-21 00:02:26 -0500

    duplicated amdjs-tests into repo... will remove this and request pull when we're closer to finished

---
+++ amdjs-tests/impl/includejs/config.js
@@ -0,0 +1,12 @@
+var config = sofea.include,
+    go = sofea.include,
+    implemented = {
+		basic: true,
+		anon: true,
+		funcString: true,
+        namedWrapped: true,
+		require: true,
+		plugins: false 
+		//pluginDynamic: true
+    };
+require = undefined;
